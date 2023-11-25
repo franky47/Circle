e7ecd8e51c4c9771a472900cd10d7f43b3983c56
Francois Best <github@francoisbest.com>
2023-11-25 13:30:01 +0100

    Initial Commit

---
+++ Circle/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Franky on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
