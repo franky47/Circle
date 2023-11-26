20fbdeed8aae889ab2970bc915d0c36deac0c848
Francois Best <github@francoisbest.com>
2023-11-26 01:52:39 +0100

    feat: Circle is now draggable

--- Circle/CircleApp.swift
+++ Circle/CircleApp.swift
@@ -9,7 +9,60 @@ struct CircleView: View {
     }
 }
 
-class CircleWindow: NSWindow {
+// class CircleWindow: NSWindow {
+//     override var contentView: NSView? {
+//         didSet {
+//             guard let contentView = contentView else { return }
+//             contentView.wantsLayer = true
+//             contentView.layer?.cornerRadius = contentView.frame.width / 2
+//             contentView.layer?.masksToBounds = true
+//         }
+//     }
+// }
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentRect = NSRect(x: 0, y: 0, width: 480, height: 300)
+        window = DraggableWindow(contentRect: contentRect, styleMask: [.borderless], backing: .buffered, defer: false)
+        window.isOpaque = false
+        window.backgroundColor = NSColor.clear
+        window.hasShadow = false
+        window.center()
+        window.contentView = NSHostingView(rootView: ContentView())
+        window.makeKeyAndOrderFront(nil)
+    }
+}
+
+class DraggableWindow: NSWindow {
+    var initialLocation: NSPoint?
+
+    override func mouseDown(with event: NSEvent) {
+        self.initialLocation = event.locationInWindow
+    }
+
+    override func mouseDragged(with event: NSEvent) {
+        let screenVisibleFrame = NSScreen.main!.visibleFrame
+        let windowFrame = self.frame
+        var newOrigin = windowFrame.origin
+
+        // Get the mouse location in window coordinates.
+        let currentLocation = event.locationInWindow
+
+        // Update the origin with the difference between the new mouse location and the old mouse location.
+        newOrigin.x += (currentLocation.x - initialLocation.x)
+        newOrigin.y += (currentLocation.y - initialLocation.y)
+
+        // Don't let window get dragged up under the menu bar
+        if ((newOrigin.y + windowFrame.size.height) > (screenVisibleFrame.origin.y + screenVisibleFrame.size.height)) {
+            newOrigin.y = screenVisibleFrame.origin.y + (screenVisibleFrame.size.height - windowFrame.size.height);
+        }
+
+        // Move the window to the new location
+        self.setFrameOrigin(newOrigin)
+    }
+
     override var contentView: NSView? {
         didSet {
             guard let contentView = contentView else { return }
@@ -17,21 +70,6 @@ class CircleWindow: NSWindow {
             contentView.layer?.cornerRadius = contentView.frame.width / 2
             contentView.layer?.masksToBounds = true
         }
-    }
-}
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow!
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        let contentRect = NSRect(x: 0, y: 0, width: 480, height: 300)
-        window = NSWindow(contentRect: contentRect, styleMask: [.borderless], backing: .buffered, defer: false)
-        window.isOpaque = false
-        window.backgroundColor = NSColor.clear
-        window.hasShadow = false
-        window.center()
-        window.contentView = NSHostingView(rootView: ContentView())
-        window.makeKeyAndOrderFront(nil)
     }
 }
 
